7fd1cd71c6e435756611977c772387b3bc0978eb
Logon Aluno <logonaluno@local.com>
2019-09-02 11:58:37 -0300

    Anotações C#

---
+++ 05-Fiap.Web.AspNet/05-Fiap.Web.AspNet/Persistences/AllZooContext.cs
@@ -0,0 +1,21 @@
+﻿using _05_Fiap.Web.AspNet.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _05_Fiap.Web.AspNet.Persistences
+{
+    public class AllZooContext : DbContext
+    {
+
+        public DbSet<Zoo> Zoos { get; set; }
+
+        //construtor - ctor tab tab
+        public AllZooContext(DbContextOptions op) : base (op)
+        {
+
+        }
+    }
+}
